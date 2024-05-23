5068ced6f24dd90b0247875c7f68efb671d50bed
rodme02 <rodrigomedeiros2002@gmail.com>
2024-05-23 14:20:53 -0300

    mudando alguns nomes

--- app.py
+++ app.py
@@ -3,26 +3,30 @@ from botocore.exceptions import ClientError
 from flask import Flask, request, jsonify
 import logging
 
+# Inicializando o Flask
 app = Flask(__name__)
 
-# Initialize DynamoDB
+# Inicializando o DynamoDB
 dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
 table = dynamodb.Table('UsersTable')
 
-# Configure logger
+# Configurando o logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+# Função para lidar com exceções do DynamoDB
 def handle_dynamodb_exception(e):
     logger.error(f"An error occurred: {e}")
     return jsonify({'error': str(e)}), 500
 
+# Função para validar os dados do usuário
 def validate_user_data(user_data, fields):
     missing_fields = [field for field in fields if field not in user_data]
     if missing_fields:
         return False, {'error': f"{', '.join(missing_fields)} are required fields"}, 400
     return True, None, None
 
+# Rota para verificar a saúde da aplicação
 @app.route('/health', methods=['GET'])
 def health():
     healthy = {'DynamoDB': 'Healthy'}
@@ -33,10 +37,11 @@ def health():
         return handle_dynamodb_exception(e)
     
     status = 'Healthy' if all(status == 'Healthy' for status in healthy.values()) else 'Unhealthy'
-    return jsonify({'message': status, 'details': healthy}), 200 if status == 'Healthy' else 500
+    return jsonify({'details': healthy}), 200 if status == 'Healthy' else 500
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
+# Rota para adicionar um usuário
+@app.route('/create_user', methods=['POST'])
+def create_user():
     try:
         user_data = request.json
         valid, error_response, status_code = validate_user_data(user_data, ['id', 'name'])
@@ -49,11 +54,12 @@ def add_user():
 
         item = {'id': user_id, 'name': user_data['name']}
         response = table.put_item(Item=item)
-        return jsonify({'message': 'User added successfully', 'response': response}), 200
+        return jsonify({'message': 'User created successfully', 'response': response}), 200
     
     except ClientError as e:
         return handle_dynamodb_exception(e)
 
+# Rota para recuperar um usuário
 @app.route('/get_user/<int:id>', methods=['GET'])
 def get_user(id):
     try:
@@ -67,6 +73,7 @@ def get_user(id):
     except ClientError as e:
         return handle_dynamodb_exception(e)
 
+# Rota para atualizar um usuário
 @app.route('/update_user', methods=['PUT'])
 def update_user():
     try:
@@ -82,6 +89,7 @@ def update_user():
     except ClientError as e:
         return handle_dynamodb_exception(e)
 
+# Rota para deletar um usuário
 @app.route('/delete_user/<int:id>', methods=['DELETE'])
 def delete_user(id):
     try:
@@ -91,5 +99,6 @@ def delete_user(id):
     except ClientError as e:
         return handle_dynamodb_exception(e)
 
+# Rodando a aplicação em HTTP
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
